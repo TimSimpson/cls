db121a4433adbcf02951b6b6e08df74e50c530ec
Tim Simpson <timsimpson4@gmail.com>
2020-05-17 13:37:55 -0500

    adds basic mypy and flake8 linting, black formating

--- clearscreen/cli.py
+++ clearscreen/cli.py
@@ -11,8 +11,9 @@ class InvalidChoice(BaseException):
     def __init__(self, arg: t.Union[int, str]) -> None:
         self.arg = arg
 
+
 def format_vertical_lines(lines: t.List[str]) -> t.List[str]:
-    is_empty = [l.strip() == '' for l in lines]
+    is_empty = [line.strip() == "" for line in lines]
     try:
         first_non_empty = is_empty.index(False)
     except ValueError:
@@ -24,19 +25,21 @@ def format_vertical_lines(lines: t.List[str]) -> t.List[str]:
     except ValueError:
         last_non_empty = len(lines)
 
-    return [''] + lines[first_non_empty: last_non_empty] + ['']
+    return [""] + lines[first_non_empty:last_non_empty] + [""]
 
 
 class Screens:
     def __init__(self, path: pathlib.Path) -> None:
         self._path = path
-        self._files = [f for f in path.iterdir() if f.is_file() and f.suffix == '.txt']
+        self._files = [
+            f for f in path.iterdir() if f.is_file() and f.suffix == ".txt"
+        ]
 
     def get_by_index(self, index: int) -> t.List[str]:
         if index > len(self._files) - 1:
             raise InvalidChoice(index)
         with open(self._files[index]) as f:
-            lines = list(l.rstrip() for l in f.readlines())
+            lines = list(line.rstrip() for line in f.readlines())
             return format_vertical_lines(lines)
 
     def get_by_name(self, name: str) -> t.List[str]:
@@ -60,13 +63,24 @@ class Screens:
         return self.get_by_index(choice)
 
 
-
-def main()-> None:
+def main() -> None:
     screens = Screens(pathlib.Path(art_module.__file__).parent)
-    parser = argparse.ArgumentParser("cls", description="Shows random art to clear the screen.")
-    parser.add_argument("--index", help=f"Index of art to pick (max {screens.length - 1}", required=False, default=None, type=int)
-    parser.add_argument("--name", required=False, default=None, help="Name of art")
-    parser.add_argument("--ls", help="List all the screens", action="store_true")
+    parser = argparse.ArgumentParser(
+        "cls", description="Shows random art to clear the screen."
+    )
+    parser.add_argument(
+        "--index",
+        help=f"Index of art to pick (max {screens.length - 1}",
+        required=False,
+        default=None,
+        type=int,
+    )
+    parser.add_argument(
+        "--name", required=False, default=None, help="Name of art"
+    )
+    parser.add_argument(
+        "--ls", help="List all the screens", action="store_true"
+    )
     args = parser.parse_args()
 
     if args.ls:
@@ -82,18 +96,20 @@ def main()-> None:
         else:
             art = screens.random()
     except InvalidChoice as ic:
-        print('Invalid selection "{}". Pick a number from 0 - {}.'
-              .format(ic.arg, screens.length))
+        print(
+            'Invalid selection "{}". Pick a number from 0 - {}.'.format(
+                ic.arg, screens.length
+            )
+        )
         sys.exit(1)
 
     for index, line in enumerate(art):
-        if index == 0 and line.strip() != '':
+        if index == 0 and line.strip() != "":
             print()  # add an extra line
         print(line)
-        if index == len(art) - 1 and line.strip() != '':
+        if index == len(art) - 1 and line.strip() != "":
             print()  # add one last line for spacing
 
 
-
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
